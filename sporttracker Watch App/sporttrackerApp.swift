00a1661fbc1509403482674c29fc9a287d03cbad
Pyry Lahtinen <pyry@pyry.info>
2024-07-19 11:50:16 +0300

    Initial Commit

---
+++ sporttracker Watch App/sporttrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sporttrackerApp.swift
+//  sporttracker Watch App
+//
+//  Created by Pyry Lahtinen on 19.7.2024.
+//
+
+import SwiftUI
+
+@main
+struct sporttracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sporttracker/sporttrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sporttrackerApp.swift
+//  sporttracker
+//
+//  Created by Pyry Lahtinen on 19.7.2024.
+//
+
+import SwiftUI
+
+@main
+struct sporttrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
